b8b1f3bb67534c5d8d8ade30690cd356750a6352
Alagapie <abdulbasitsuleiman660@gmail.com>
2024-09-24 01:11:40 +0100

    Update second_page.py

--- mvp/second_page.py
+++ mvp/second_page.py
@@ -14,7 +14,7 @@ from openpyxl import Workbook
 app = Flask(__name__)
 
 # Load YOLO model
-model = YOLO('best.pt')
+model = YOLO('v10.pt')
 
 # Store annotations and annotation images in memory
 annotations = {}
